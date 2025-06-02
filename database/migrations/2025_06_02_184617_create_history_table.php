eb58187e7969bba8f7adcac6e1fa6a27654c9b61
Otmane Boumediane <boumediane.othmane@gmail.com>
2025-06-02 19:50:17 +0100

    first commit

---
+++ database/migrations/2025_06_02_184617_create_history_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->id();
+            $table->integer('active_power_production');
+            $table->integer('active_power_grid');
+            $table->integer('active_power_load');
+            $table->integer('active_power_battery');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('history');
+    }
+};
