d3ce242303a8e9c24ba8c409728e963d6e179e3e
Otmane Boumediane <boumediane.othmane@gmail.com>
2025-06-03 20:36:58 +0100

    add monitoring page

---
+++ resources/js/pages/monitoring.tsx
@@ -0,0 +1,47 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+import { UtilityPole } from 'lucide-react';
+import 'reactflow/dist/style.css';
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Monitoring',
+        href: '/monitoring',
+    },
+];
+
+export default function Monitoring() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Monitoring" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-lg p-4">
+                <div className="grid grid-cols-2 gap-4">
+                    <Card>
+                        <CardContent className="space-y-4">
+                            <div className="flex justify-between">
+                                <div className="flex gap-x-4">
+                                    <span className="grid size-12 place-items-center rounded-lg bg-blue-600 text-white">
+                                        <UtilityPole className="size-6" />
+                                    </span>
+
+                                    <div>
+                                        <h3 className="text-xl font-bold">Utility Grid</h3>
+                                        <p className="text-sm text-gray-700">Consuming</p>
+                                    </div>
+                                </div>
+
+                                <div className="text-xl font-bold" style={{ fontFamily: 'Inter, sand-serif' }}>
+                                    2770 W
+                                </div>
+                            </div>
+
+                            <Progress value={80} className="h-3" />
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('monitoring', function () {
+        return Inertia::render('monitoring');
+    });
 });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
