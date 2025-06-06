7968948ecb93fc5f2ea91c03c0c00fb8eac006e2
Otmane Boumediane <boumediane.othmane@gmail.com>
2025-06-06 17:07:58 +0100

    add battery usage card

--- resources/js/pages/ProductionChart.tsx
+++ resources/js/pages/ProductionChart.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { TrendingUp } from 'lucide-react';
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { History } from '@/types';
 
@@ -57,16 +56,6 @@ export function ChartLineMultiple({ data }: { data: History[] }) {
                     </LineChart>
                 </ChartContainer>
             </CardContent>
-            <CardFooter>
-                <div className="flex w-full items-start gap-2 text-sm">
-                    <div className="grid gap-2">
-                        <div className="flex items-center gap-2 leading-none font-medium">
-                            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                        </div>
-                        <div className="text-muted-foreground flex items-center gap-2 leading-none">Showing total visitors for the last 6 months</div>
-                    </div>
-                </div>
-            </CardFooter>
         </Card>
     );
 }
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,4 +1,7 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
 import AppLayout from '@/layouts/app-layout';
 import { History, type BreadcrumbItem } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
@@ -27,18 +30,42 @@ export default function Dashboard() {
             <Head title="Dashboard" />
 
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <h2 className="text-xl font-semibold tracking-tight">Dashboard</h2>
+                <div className="flex items-center justify-between">
+                    <h2 className="text-xl font-semibold tracking-tight">Dashboard</h2>
+
+                    <div className="flex gap-4">
+                        <Input type="date" />
+                        <Input type="date" />
+                        <Button>Export</Button>
+                    </div>
+                </div>
 
                 <div className="grid grid-cols-3 gap-4">
                     <div className="col-span-2">
                         <ChartLineMultiple data={props.data} />
                     </div>
 
-                    <div className="col-span-1">
-                        <Card className="h-[505px]">
+                    <div className="col-span-1 space-y-4">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Battery usage</CardTitle>
+                            </CardHeader>
+
+                            <CardContent className="space-y-4">
+                                <div className="flex items-center justify-between gap-x-4">
+                                    <div className="text-3xl font-bold" style={{ fontFamily: 'Inter, sand-serif' }}>
+                                        22%
+                                    </div>
+                                    <div className="text-sm text-gray-500">75 minutes left estimated</div>
+                                </div>
+
+                                <Progress value={22} className="h-4 rounded" />
+                            </CardContent>
+                        </Card>
+
+                        <Card className="h-[464px]">
                             <CardHeader>
                                 <CardTitle>Power Flow</CardTitle>
-                                <CardDescription>Live energy usage and sources.</CardDescription>
                             </CardHeader>
 
                             <CardContent className="h-full">
